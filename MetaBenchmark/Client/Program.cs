1c65c47d07474946921cbeff2c931eac4c366bf0
Brian Barrett <blaxbb@gmail.com>
2021-07-07 18:10:45 -0400

    Fix public api calls failing, layout improvements

--- MetaBenchmark/Client/Program.cs
+++ MetaBenchmark/Client/Program.cs
@@ -21,10 +21,13 @@ namespace MetaBenchmark.Client
             builder.Services.AddHttpClient("MetaBenchmark.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
+            builder.Services.AddHttpClient("public", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("MetaBenchmark.ServerAPI"));
 
             builder.Services.AddApiAuthorization();
+
 
             await builder.Build().RunAsync();
         }
