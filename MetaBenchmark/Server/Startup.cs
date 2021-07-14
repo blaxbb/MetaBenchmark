85e65f74cc038db400ed17f89901e6fc24b6fc15
Brian Barrett <blaxbb@gmail.com>
2021-07-14 03:20:28 -0400

    Disable swagger in prod

--- MetaBenchmark/Server/Startup.cs
+++ MetaBenchmark/Server/Startup.cs
@@ -80,6 +80,11 @@ namespace MetaBenchmark.Server
                 app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
                 app.UseWebAssemblyDebugging();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "MetaBenchmark");
+                });
             }
             else
             {
@@ -87,11 +92,6 @@ namespace MetaBenchmark.Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MetaBenchmark");
-            });
 
             //DataImporter.Import(db);
 
