951ede4d547154cbf70b5a67648e29e20168eff7
Brian Barrett <blaxbb@gmail.com>
2021-08-18 04:22:42 -0400

    Changed landing page to display product list, with navigation links

---
+++ MetaBenchmark/Client/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetaBenchmark.Client
+{
+    public static class ListExtensions
+    {
+        [ThreadStatic]
+        private static Random rand = new System.Random();
+        public static IEnumerable<T> Random<T>(this List<T> list)
+        {
+            return list.OrderBy(i => rand.Next());
+        }
+
+        public static T RandomItem<T>(this List<T> list)
+        {
+            var index = rand.Next(list.Count);
+            return list[index];
+        }
+    }
+}
