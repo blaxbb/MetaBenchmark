ed3f7c504b14e6a0e565057f652281ecf0fdfc47
Brian Barrett <blaxbb@gmail.com>
2021-11-08 06:01:08 -0500

    Rewrote compare products page to better segment product benchmarks between sources

--- MetaBenchmark/Server/Data/ApplicationDbContext.cs
+++ MetaBenchmark/Server/Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using IdentityServer4.EntityFramework.Options;
+﻿using Duende.IdentityServer.EntityFramework.Options;
 using MetaBenchmark.Shared;
 using MetaBenchmark.Server.Models;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
